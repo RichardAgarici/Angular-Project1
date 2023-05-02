595cff6283c09778ebef51d2d63ffed86dedc476
RichardAgarici <laurentiu.agarici@gmail.com>
2023-05-02 15:57:11 +0200

    adding a few changes to the components

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: '<app-server></app-server><app-server></app-server>',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
