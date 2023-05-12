84d5ec98970454e2450d29a27459fff3c311d1c6
RichardAgarici <laurentiu.agarici@gmail.com>
2023-05-12 17:28:41 +0200

    update-ing the 2 way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
   selector: '.app-servers',
-  templateUrl: `servers.component.html`,
+  templateUrl: `./servers.component.html`,
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowingNewServer = false;
+  serverCreationStatus = 'No Server was created.'
+  serverName = 'TestServer'
 
+  constructor() {
+    setTimeout(() => {
+      this.allowingNewServer = true
+    }, 2000)
+  }
+  ngOnInit() {
+  }
+// aici odata ce am implementat OnInit, trebuie metoda ngOnInit(){} overrightuita.
+  onCreateServer() {
+    return this.serverCreationStatus = 'Server was created!'
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
 
